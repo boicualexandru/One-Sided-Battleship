941bdfb600e0aaca97f6f354f4d4874e83b2894e
Alex Boicu <boicualexandru@mail.com>
2019-08-26 22:38:51 +0300

    Unit tests for Game and Map classes

---
+++ Battleships.Tests/GameTest.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using Xunit;
+
+namespace Battleships.Tests
+{
+    public class GameTest
+    {
+        [Fact]
+        public void Start_ShouldCallMapLoading()
+        {
+            // Arrange
+            var mapMock = new Mock<IMap>();
+            var uiMock = new Mock<IUserInterface>();
+
+            var game = new Game(mapMock.Object, uiMock.Object);
+            // Act
+            game.Start();
+
+            // Assert
+            mapMock.Verify(map => map.Load(), Times.AtLeastOnce());
+        }
+    }
+}
---
+++ Battleships.Tests/MapTest.cs
@@ -0,0 +1,49 @@
+﻿using Battleships.Enums;
+using Battleships.Factories;
+using Battleships.Models;
+using Moq;
+using Xunit;
+
+namespace Battleships.Tests
+{
+    public class MapTest
+    {
+        [Fact]
+        public void Load_ShouldRequestRandomShips()
+        {
+            // Arange
+            var mapConfiguration = new MapConfiguration
+            {
+                BattleshipsCount = 1,
+                DestroyersCount = 2,
+                Height = 10,
+                Width = 10
+            };
+
+            var battleShipFactoryMock = new Mock<IBattleShipFactory>();
+            battleShipFactoryMock
+                .Setup(battleShipFactory => battleShipFactory.GetRandomShip(mapConfiguration.Width, mapConfiguration.Height))
+                .Returns(new Ship(new Location { X = 7, Y = 2 }, Axis.Vertical, ShipType.BattleShip));
+
+            var destroyerShipFactoryMock = new Mock<IDestroyerShipFactory>();
+            destroyerShipFactoryMock
+                .SetupSequence(destroyerShipFactory => destroyerShipFactory.GetRandomShip(mapConfiguration.Width, mapConfiguration.Height))
+                .Returns(new Ship(new Location { X = 1, Y = 2 }, Axis.Horizontal, ShipType.Destroyer))
+                .Returns(new Ship(new Location { X = 1, Y = 3 }, Axis.Horizontal, ShipType.Destroyer));
+
+            var map = new Map(battleShipFactoryMock.Object, destroyerShipFactoryMock.Object, mapConfiguration);
+
+            // Act
+            map.Load();
+
+            //Assert
+            battleShipFactoryMock.Verify(
+                battleShipFactory => battleShipFactory.GetRandomShip(mapConfiguration.Width, mapConfiguration.Height), 
+                Times.Exactly(1));
+
+            destroyerShipFactoryMock.Verify(
+                destroyerShipFactory => destroyerShipFactory.GetRandomShip(mapConfiguration.Width, mapConfiguration.Height),
+                Times.Exactly(2));
+        }
+    }
+}
